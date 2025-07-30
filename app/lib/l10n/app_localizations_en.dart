00f3493fe7b8ca759557b057ab29b3bfb81b72a7
Takami Marsh <t-marsh@kttprojects.com>
2025-07-30 11:55:22 +0900

    Improve email validation

--- app/lib/l10n/app_localizations_en.dart
+++ app/lib/l10n/app_localizations_en.dart
@@ -9,10 +9,6 @@ class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
-  String get onlyGoogleEmailAllowed =>
-      'Only Gmail accounts are supported at this time';
-
-  @override
   String get checkSpamJunk =>
       'Please check your spam/junk folder if you don\'t see the email in your inbox';
 
